d14c24975754499ff539df482aeb3440bb523d6e
Nurdin <mrsunnysummer@gmail.com>
2024-11-08 01:49:32 +0700

    changes: update dependencies import for camera activity and image classifier helper

--- app/src/main/java/com/dicoding/picodiploma/mycamera/ImageClassifierHelper.kt
+++ app/src/main/java/com/dicoding/picodiploma/mycamera/ImageClassifierHelper.kt
@@ -13,8 +13,8 @@ import org.tensorflow.lite.support.image.TensorImage
 import org.tensorflow.lite.support.image.ops.ResizeOp
 import org.tensorflow.lite.task.core.BaseOptions
 import org.tensorflow.lite.task.core.vision.ImageProcessingOptions
-import org.tensorflow.lite.task.vision.classifier.Classifications
-import org.tensorflow.lite.task.vision.classifier.ImageClassifier
+import org.tensorflow.lite.task.gms.vision.classifier.Classifications
+import org.tensorflow.lite.task.gms.vision.classifier.ImageClassifier
 import java.lang.IllegalStateException
 
 class ImageClassifierHelper(
