6765b3fbddd06497871b384eaf889a18f57cff7f
Nurdin <mrsunnysummer@gmail.com>
2024-11-07 22:05:39 +0700

    chore - style: add tflite model and styling camerax

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,10 +32,13 @@ android {
     }
     buildFeatures {
         viewBinding = true
+        mlModelBinding = true
     }
 }
 
 dependencies {
+    implementation("org.tensorflow:tensorflow-lite-support:0.1.0")
+    implementation("org.tensorflow:tensorflow-lite-metadata:0.1.0")
     val cameraxVersion = "1.3.0"
 
     implementation("androidx.core:core-ktx:1.12.0")
